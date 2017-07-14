532ef7ca1e717b974951f6256dbdc903fcbb7b60
Cody Daniels <codydaniels@gmail.com>
2017-07-13 22:51:04 -0700

    Add setting to change background

--- options.js
+++ options.js
@@ -1,22 +1,37 @@
 var nameField = document.getElementById('name');
 var dateField = document.getElementById('date');
+var bkgrndSelect = document.querySelector('select');
 
 function updateName() {
-  chrome.storage.sync.set({'name': nameField.value}, function() {
-    console.log('Saved!');
+  chrome.storage.sync.set({
+    'name': nameField.value
   });
 }
 
 function updateDate() {
-  chrome.storage.sync.set({'date': dateField.value}, function() {
-    console.log('Saved!');
+  chrome.storage.sync.set({
+    'date': dateField.value
+  });
+}
+
+function updateBkgrnd() {
+  chrome.storage.sync.set({
+    'bkgrnd': bkgrndSelect.value
+  }, function() {
+    console.log(bkgrndSelect.value);
   });
 }
 
 nameField.addEventListener("change", updateName);
 dateField.addEventListener("change", updateDate);
+bkgrndSelect.addEventListener("change", updateBkgrnd);
 
-chrome.storage.sync.get(['date', 'name'], function(data) {
+chrome.storage.sync.get(['date', 'name', 'bkgrnd'], function(data) {
   nameField.value = data.name;
   dateField.value = data.date;
+  if(data.bkgrnd !== undefined) {
+    bkgrndSelect.value = data.bkgrnd;
+  } else {
+    bkgrndSelect.value = 'orange';
+  }
 });
